e439e0af33a53510765d679f31f9739ee374bf64
Paola Morales <pamoralessgarcia@gmail.com>
2025-06-24 17:21:52 -0600

    Agregar header

---
+++ header.php
@@ -0,0 +1,48 @@
+<?php
+    //session_start();
+    
+?>
+
+<div class="fondo_menu bg-dark">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a class="navbar-brand" href="#">Inicio</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                
+                <?php if(empty($_SESSION['usuario'])): ?>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="visualizadortareas.php">Productos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Precios</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contactanos</a>
+                        </li>
+                    </ul>
+                    <a href="manejousuarios/login.php" class="boton">Inicia Session</a>
+                    <a href="manejousuarios/registro.php" class="boton">Registrate</a>
+                    <a href="manejousuarios/modo_invitado.php" class="boton">Registrate</a>
+                </div>
+                <?php else: ?>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Agregar informacion</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Editar perfil</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Session de recursos</a>
+                        </li>
+                    </ul>
+                    <a href="manejousuarios/logout.php" class="boton">Cerrar Sesion</a>
+                </div>
+                <?php endif ?>
+        </nav>
+</div>
+<div class="fondo">
--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 // Si ya está logeado o es invitado, se redirige al visualizadortareas.php
 if ((isset($_SESSION['usuario']) && isset($_SESSION['id_usuario'])) || (isset($_SESSION['es_invitado']) && $_SESSION['es_invitado'] === true)){
     header("Location: visualizadortareas.php");
-    exit();
+    //exit();
 }
 ?>
 
@@ -22,46 +22,56 @@ if ((isset($_SESSION['usuario']) && isset($_SESSION['id_usuario'])) || (isset($_
 <head>
     <meta charset="UTF-8">
     <title>Bienvenido - Tareas</title>
+    
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap JS Bundle-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
     <link rel="stylesheet" href="css/estilos.css">
 </head>
-<body class="pagina-index">
-    <h2> Bienvenido a la Lista de Tareas</h2>
+<body>
+    <?php require_once("header.php"); ?>
+    
+    <div class="pagina-index">
 
-    <!--Formulario de Registro -->
-   
-    <form action="manejousuarios/registro.php" method="post">
-        <h3>Registarse</h3>
-        <input type="text" name="nombre_usuario" placeholder="Nombre de usuario" required />
-        <input type="password" name="contrasena" placeholder="Contraseña" required />
-        <button type="submit">Crear cuenta</button>
-    </form>
-   
-    <?php if(!empty($registro_error)): ?>
-        <div style="color: red; text-align: center; margin-bottom: 15px;">
-            <?= htmlspecialchars($registro_error) ?>
-        </div>
-    <?php endif; ?>
+        <h2> Bienvenido a la Lista de Tareas</h2>
+        <!--Formulario de Registro -->
+    
+        <form action="manejousuarios/registro.php" method="post">
+            <h3>Registarse</h3>
+            <input type="text" name="nombre_usuario" placeholder="Nombre de usuario" required />
+            <input type="password" name="contrasena" placeholder="Contraseña" required />
+            <button type="submit">Crear cuenta</button>
+        </form>
+    
+        <?php if(!empty($registro_error)): ?>
+            <div style="color: red; text-align: center; margin-bottom: 15px;">
+                <?= htmlspecialchars($registro_error) ?>
+            </div>
+        <?php endif; ?>
 
-    <!-- Formulario de Inicio de Sesión -->
-    <form action="manejousuarios/login.php" method="post">
-        <h3>Iniciar sesión</h3>
-        <input type="text" name="nombre_usuario" placeholder="Nombre de Usuario" required />
-        <input type="password" name="contrasena" placeholder="Contraseña" required />
-        <button type="submit">Entrar</button>
-    </form>
+        <!-- Formulario de Inicio de Sesión -->
+        <form action="manejousuarios/login.php" method="post">
+            <h3>Iniciar sesión</h3>
+            <input type="text" name="nombre_usuario" placeholder="Nombre de Usuario" required />
+            <input type="password" name="contrasena" placeholder="Contraseña" required />
+            <button type="submit">Entrar</button>
+        </form>
 
-    <?php if(!empty($login_error)): ?>
-        <div style="color: red; text-align: center; margin-bottom: 15px;">
-            <?= htmlspecialchars($login_error) ?>
-        </div>
-    <?php endif; ?>
+        <?php if(!empty($login_error)): ?>
+            <div style="color: red; text-align: center; margin-bottom: 15px;">
+                <?= htmlspecialchars($login_error) ?>
+            </div>
+        <?php endif; ?>
 
-    <!--Botón para modo invitado -->
-    <form action ="manejousuarios/modo_invitado.php"  method="post">
-        <button type="submit" class="btn-invitado">Entrar como invitado</button>
-    </form>
-    <footer style="background-color: #f2f2f2; text-align: center; padding: 15px; margin-top: 30px;"> <!-- seria nuestro cuadro donde aparecerian nuestros nombres-->
-        <p>Equipo: <br> Erick Mauricio Artega Velázquez, Paola Alejandra Morales García, Miguel Ángel Ramírez Aguilar, Manuel Antonio Gutierrez López</p>
-    </footer>
+        <!--Botón para modo invitado -->
+        <form action ="manejousuarios/modo_invitado.php"  method="post">
+            <button type="submit" class="btn-invitado">Entrar como invitado</button>
+        </form>
+        <footer style="background-color: #f2f2f2; text-align: center; padding: 15px; margin-top: 30px;"> <!-- seria nuestro cuadro donde aparecerian nuestros nombres-->
+            <p>Equipo: <br> Erick Mauricio Arteaga Velázquez, Paola Alejandra Morales García, Miguel Ángel Ramírez Aguilar, Manuel Antonio Gutiérrez López</p>
+        </footer>
+    </div>
 </body>
 </html>
