85eb5dd094556117eee13f1968c5ac8979a2e42b
Paola Morales <pamoralessgarcia@gmail.com>
2025-06-24 17:51:32 -0600

    agregar logo a header

--- header.php
+++ header.php
@@ -5,7 +5,8 @@
 
 <div class="fondo_menu bg-dark">
         <nav class="navbar navbar-expand-lg navbar-dark">
-                <a class="navbar-brand" href="index.php">Inicio</a>
+                <a class="navbar-brand" href="index.php"><img src="img/organiza_tareas.jpeg" alt="Logo" width="30" height="30" class="d-inline-block align-text-top me-2">
+                TaskFlow</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
